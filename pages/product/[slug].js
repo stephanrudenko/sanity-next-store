fbc6e95b5d97434a4248606fe196aa837bd567c0
Stepan Rudenko <s.rudenko584@gmail.com>
2022-11-01 20:02:35 +0200

    added Cart and implemented Navbar. added dynamic routes for products

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,10 +7,12 @@ import {
   AiOutlineStar,
 } from "react-icons/ai";
 import { Product } from "../../components";
+import { useStateContext } from "../../context/StateContext";
 
 const ProductDetails = ({ product, similarProducts }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty, onAdd } = useStateContext();
 
   return (
     <div>
@@ -52,19 +54,23 @@ const ProductDetails = ({ product, similarProducts }) => {
           <div className="quantity">
             <h3>Quantity: </h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
               <span className="num" onClick="">
-                0
+                {qty}
               </span>
-              <span className="plus" onClick="">
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">
@@ -95,7 +101,6 @@ export const getStaticPaths = async () => {
     }`;
 
   const arrPaths = await client.fetch(query);
-  console.log(arrPaths);
 
   const paths = arrPaths.map((product) => ({
     params: {
